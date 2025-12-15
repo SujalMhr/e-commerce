bdca9053e5236fbcc5432837eef2c56e2f96660e
SujalMhr <ssuuzzaall.mhr8@gmail.com>
2025-12-15 16:19:30 +0545

    update live link

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://e-commerce-rho-rosy-45.vercel.app/"
+    ? "https://e-commerce-rho-rosy-45.vercel.app"
     : "http://localhost:4000";
